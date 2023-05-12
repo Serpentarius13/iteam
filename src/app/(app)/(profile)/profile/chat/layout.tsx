c728b28deccace4b723e2d67e883f94c8e92952c
Serpentarius13 <serpentarium13@mail.ru>
2023-05-12 22:50:47 +0300

    !

--- src/app/(app)/(profile)/profile/chat/layout.tsx
+++ src/app/(app)/(profile)/profile/chat/layout.tsx
@@ -1,23 +1,24 @@
+"use client";
+
 import LoadingScreen from "@/components/Shared/Load/LoadingScreen";
 
 import { ReactNode } from "react";
 import FriendsSidebar from "@/components/Profile/FriendsSidebar";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { SidebarFriend } from "@/lib/types/utility";
 
-async function getFriends() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const response = await fetch(`${baseUrl}/api/friends`);
+export const revalidate = 0;
 
-  if (response.ok) {
-    return await response.json();
-  } else return null;
-}
+export default function ChatLayout({ children }: { children: ReactNode }) {
+  const { data: friends, isLoading } = useQuery<SidebarFriend[]>({
+    queryFn: async () => {
+      const { data } = await axios.get("/api/friends");
+      return data;
+    },
+    queryKey: ["friends"],
+  });
 
-export default async function ChatLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  const friends = await getFriends();
   return (
     <main className="flex h-full w-full items-start justify-between">
       <FriendsSidebar friends={friends ?? []}></FriendsSidebar>
